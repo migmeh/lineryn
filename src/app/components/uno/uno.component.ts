338bca21a4485503403716241c6ca8fb697cd8a7
migmeh <xmigmeh@gmail.com>
2024-10-18 01:03:30 -0600

    fix monile

--- src/app/components/uno/uno.component.ts
+++ src/app/components/uno/uno.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var $: any;
 @Component({
   selector: 'app-uno',
   templateUrl: './uno.component.html',
@@ -9,7 +9,16 @@ export class UnoComponent implements OnInit {
 
   constructor() { }
 
+
+  aceptCookie(){
+    let mycookie = localStorage.setItem("mycookie", "yes");
+  }
+
   ngOnInit(): void {
+    if(localStorage.getItem("mycookie") === "yes"){
+      $("#mycookie").hide();
+    }
+
   }
 
 }
